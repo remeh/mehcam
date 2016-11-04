81890ce8450911316c1522f048708a765bc484e6
remeh <remeh@remeh.fr>
2016-11-04 23:03:36 +0100

    main: get image from cam, apply hash+distance, store img on big distances.

---
+++ image.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"bytes"
+	"fmt"
+	"image"
+	_ "image/gif"
+	_ "image/jpeg"
+	_ "image/png"
+	"io/ioutil"
+	"net/http"
+
+	"github.com/jteeuwen/imghash"
+)
+
+func hash(data []byte) (uint64, error) {
+	buff := bytes.NewBuffer(data)
+
+	img, format, err := image.Decode(buff)
+	if err != nil {
+		return 0, err
+	}
+
+	avg := imghash.Average(img)
+
+	return avg, nil
+}
+
+func get() ([]byte, error) {
+	req, err := http.NewRequest("GET", env.url, nil)
+	if err != nil {
+		return nil, err
+	}
+
+	req.Header.Add("Authorization", env.auth)
+
+	var resp *http.Response
+
+	if resp, err = http.DefaultClient.Do(req); err != nil {
+		return nil, err
+	}
+
+	if resp.StatusCode != 200 {
+		return nil, fmt.Errorf("status: %d", resp.StatusCode)
+	}
+
+	defer resp.Body.Close()
+	return ioutil.ReadAll(resp.Body)
+}
---
+++ main.go
@@ -0,0 +1,64 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"os"
+	"time"
+
+	"github.com/jteeuwen/imghash"
+)
+
+var env struct {
+	auth string
+	url  string
+}
+
+func main() {
+	// read environment var
+
+	env.auth = os.Getenv("AUTH")
+	env.url = os.Getenv("URL")
+
+	// ----------------------
+
+	var lastHash, currHash uint64
+	var lastImg, currImg []byte
+	var err error
+
+	for {
+		currHash, currImg, err = currentHash()
+		if err != nil {
+			fmt.Println("can't retrieve hash:", err)
+		}
+
+		dist := imghash.Distance(lastHash, currHash)
+
+		if dist > 10 {
+			fmt.Println("detected a distance:", dist)
+			if err = ioutil.WriteFile("/tmp/"+now(), lastImg, 0644); err != nil {
+				fmt.Println("while writing file:", err)
+			}
+		}
+
+		lastImg, lastHash = currImg, currHash
+		time.Sleep(time.Second * 1)
+	}
+}
+
+func currentHash() (uint64, []byte, error) {
+	data, err := get()
+	if err != nil {
+		return 0, nil, fmt.Errorf("error while hashing the image: %s", err.Error())
+	}
+
+	if h, err := hash(data); err != nil {
+		return 0, nil, fmt.Errorf("error while hashing the image: %s", err.Error())
+	} else {
+		return h, data, err
+	}
+}
+
+func now() string {
+	return time.Now().Format("2006-01-02-15-04-05.jpg")
+}
