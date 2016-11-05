43b37832f1a0d14678a6f37227b23718bfd24219
remeh <remeh@remeh.fr>
2016-11-05 14:44:54 +0100

    main: dist as environment config + comments.

--- image.go
+++ image.go
@@ -13,6 +13,33 @@ import (
 	"github.com/jteeuwen/imghash"
 )
 
+// get queries the cam webserver for the image.
+// It provides authentification through an HTTP
+// header set in the env.
+func get() ([]byte, error) {
+	req, err := http.NewRequest("GET", env.url, nil)
+	if err != nil {
+		return nil, err
+	}
+
+	req.Header.Add("Authorization", env.auth)
+
+	var resp *http.Response
+	if resp, err = http.DefaultClient.Do(req); err != nil {
+		return nil, err
+	}
+
+	if resp.StatusCode != 200 {
+		return nil, fmt.Errorf("status: %d", resp.StatusCode)
+	}
+
+	defer resp.Body.Close()
+	return ioutil.ReadAll(resp.Body)
+}
+
+// hash hashes the given bytes using the
+// Average Hash method.
+// Errors on unknown file format.
 func hash(data []byte) (uint64, error) {
 	buff := bytes.NewBuffer(data)
 
@@ -24,26 +51,4 @@ func hash(data []byte) (uint64, error) {
 	avg := imghash.Average(img)
 
 	return avg, nil
-}
-
-func get() ([]byte, error) {
-	req, err := http.NewRequest("GET", env.url, nil)
-	if err != nil {
-		return nil, err
-	}
-
-	req.Header.Add("Authorization", env.auth)
-
-	var resp *http.Response
-
-	if resp, err = http.DefaultClient.Do(req); err != nil {
-		return nil, err
-	}
-
-	if resp.StatusCode != 200 {
-		return nil, fmt.Errorf("status: %d", resp.StatusCode)
-	}
-
-	defer resp.Body.Close()
-	return ioutil.ReadAll(resp.Body)
 }
--- main.go
+++ main.go
@@ -4,21 +4,28 @@ import (
 	"fmt"
 	"io/ioutil"
 	"os"
+	"strconv"
 	"time"
 
 	"github.com/jteeuwen/imghash"
 )
 
+// env contains the configuration for the
+// execution of the app.
 var env struct {
 	auth     string
 	url      string
 	out      string
 	duration time.Duration
+	dist     uint64
 }
 
 func main() {
 
-	readParams()
+	if err := readParams(); err != nil {
+		fmt.Println("error: can't read mandatory params:", err)
+		os.Exit(1)
+	}
 
 	// ----------------------
 
@@ -28,40 +35,67 @@ func main() {
 
 	ticker := time.NewTicker(env.duration)
 
+	// timed infinite loop
 	for t := range ticker.C {
-		currHash, currImg, err = currentHash()
+
+		// retrieve the current img and its hash
+		currHash, currImg, err = current()
 		if err != nil {
 			fmt.Println("can't retrieve hash:", err)
 		}
 
+		// compute the distance between previous image and the current one
 		dist := imghash.Distance(lastHash, currHash)
-
-		if dist > 10 {
+		if dist > env.dist {
 			fmt.Println(time.Now(), "detected a distance:", dist)
-			if err = ioutil.WriteFile(env.out+now(t), lastImg, 0644); err != nil {
+			if err = ioutil.WriteFile(env.out+filename(t), lastImg, 0644); err != nil {
 				fmt.Println("while writing file:", err)
 			}
 		}
 
+		// store for next iteration
 		lastImg, lastHash = currImg, currHash
 	}
 }
 
-func readParams() {
+// readParams reads in the execution environment for some configuration
+func readParams() error {
+	var err error
+
+	// authorization and webserver url
+
 	env.auth = os.Getenv("AUTH")
 	env.url = os.Getenv("URL")
+
+	if len(env.auth) == 0 || len(env.url) == 0 {
+		return fmt.Errorf("no url or no authorization info provided.")
+	}
+
+	// output directory
+
 	env.out = os.Getenv("OUTPUT")
 	if len(env.out) > 0 && env.out[len(env.out)-1] != '/' {
 		env.out += "/"
 	}
 
-	var err error
+	// ----------------------
+
 	if env.duration, err = time.ParseDuration(os.Getenv("DURATION")); err != nil {
+		fmt.Println("warning: can't read DURATION env var. Fallback on 1s")
 		env.duration = time.Second
 	}
+
+	if env.dist, err = strconv.ParseUint(os.Getenv(""), 10, 64); err != nil {
+		fmt.Println("warning: can't read DIST env var. Fallback on 10")
+		env.dist = 10
+	}
+
+	return nil
 }
 
-func currentHash() (uint64, []byte, error) {
+// current snapshots the image currently seen by the cam.
+// It returns the hash and the image (in the original format).
+func current() (uint64, []byte, error) {
 	data, err := get()
 	if err != nil {
 		return 0, nil, fmt.Errorf("error while hashing the image: %s", err.Error())
@@ -74,6 +108,8 @@ func currentHash() (uint64, []byte, error) {
 	}
 }
 
-func now(t time.Time) string {
+// filename returns a convenient filename using the given time
+// as input.
+func filename(t time.Time) string {
 	return t.Format("2006-01-02_15-04-05.jpg")
 }
